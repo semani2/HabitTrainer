dc1343fe7814530a99a5eccbfe2ec87e04ffb0b2
Sai Prajnan Emani <sai.emani@citrix.com>
2017-12-24 16:47:08 -0500

    Clean up with extension functions

--- app/src/main/java/com/sai/habittrainer/db/HabitDbTable.kt
+++ app/src/main/java/com/sai/habittrainer/db/HabitDbTable.kt
@@ -2,11 +2,16 @@ package com.sai.habittrainer.db
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Log
 import com.sai.habittrainer.Habit
+import com.sai.habittrainer.db.HabitEntry.DESCRIPTION_COLUMN
+import com.sai.habittrainer.db.HabitEntry.IMAGE_COLUMN
+import com.sai.habittrainer.db.HabitEntry.TITLE_COLUMN
+import com.sai.habittrainer.db.HabitEntry._ID
 import java.io.ByteArrayOutputStream
 
 /**
@@ -24,9 +29,9 @@ class HabitDbTable(context: Context) {
         val values = ContentValues()
         // Using the with() scoping
         with(values) {
-            put(HabitEntry.TITLE_COLUMN, habit.title)
-            put(HabitEntry.DESCRIPTION_COLUMN, habit.description)
-            put(HabitEntry.IMAGE_COLUMN, toByteArray(habit.image))
+            put(TITLE_COLUMN, habit.title)
+            put(DESCRIPTION_COLUMN, habit.description)
+            put(IMAGE_COLUMN, toByteArray(habit.image))
         }
 
         val id = db.transaction {
@@ -38,28 +43,27 @@ class HabitDbTable(context: Context) {
     }
 
     fun readAllHabits(): List<Habit> {
-        val columns = arrayOf(HabitEntry._ID, HabitEntry.TITLE_COLUMN, HabitEntry.DESCRIPTION_COLUMN, HabitEntry.IMAGE_COLUMN)
+        val columns = arrayOf(_ID, TITLE_COLUMN, DESCRIPTION_COLUMN, IMAGE_COLUMN)
 
         val db = dbHelper.readableDatabase
 
-        val order = "${HabitEntry._ID} ASC"
+        val order = "${_ID} ASC"
 
-        val cursor = db.query(HabitEntry.TABLE_NAME, columns, null, null, null, null, order)
+        val cursor = db.doQuery(tableName = HabitEntry.TABLE_NAME, columns = columns, orderBy = order)
 
+        return parseHabitsFromCursor(cursor)
+    }
+
+    private fun parseHabitsFromCursor(cursor: Cursor): MutableList<Habit> {
         val habits = mutableListOf<Habit>()
-
-        while(cursor.moveToNext()) {
-            val title = cursor.getString(cursor.getColumnIndex(HabitEntry.TITLE_COLUMN))
-            val description = cursor.getString(cursor.getColumnIndex(HabitEntry.DESCRIPTION_COLUMN))
-            val imageByteArray = cursor.getBlob(cursor.getColumnIndex(HabitEntry.IMAGE_COLUMN))
-
-            val imageBitmap = BitmapFactory.decodeByteArray(imageByteArray, 0 ,imageByteArray.size)
+        while (cursor.moveToNext()) {
+            val title = cursor.getString(TITLE_COLUMN)
+            val description = cursor.getString(DESCRIPTION_COLUMN)
+            val imageBitmap = cursor.getBitmap(IMAGE_COLUMN)
 
             habits.add(Habit(title, description, imageBitmap))
         }
         cursor.close()
-        db.close()
-
         return habits
     }
 
@@ -85,4 +89,18 @@ private inline fun <T> SQLiteDatabase.transaction(function: SQLiteDatabase.() ->
     close()
 
     return result
+}
+
+private fun SQLiteDatabase.doQuery(tableName: String, columns: Array<String>, selection: String? = null, selectionArgs: Array<String>? = null,
+                                 groupBy: String? = null, having: String? = null, orderBy: String? = null) : Cursor{
+    return query(tableName, columns, selection, selectionArgs, groupBy, having, orderBy)
+}
+
+private fun Cursor.getString(columnName: String) = getString(getColumnIndex(columnName))
+
+private fun Cursor.getBlob(columnName: String) = getBlob(getColumnIndex(columnName))
+
+private fun Cursor.getBitmap(columnName: String) : Bitmap {
+    val imageByteArray = getBlob(columnName)
+    return BitmapFactory.decodeByteArray(imageByteArray, 0 ,imageByteArray.size)
 }
